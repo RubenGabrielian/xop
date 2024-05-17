4d6e1981903de8d9d19c6b4d6686a78a004b1218
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-05-17 23:44:23 +0400

    modal

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,21 @@
+import {MainButton, useShowPopup} from "@vkruglikov/react-telegram-web-app";
+
 export default function Header() {
+
+
+    const showPopup = useShowPopup();
+
+
+    const handleClick = () =>
+        showPopup({
+            message: 'Hello, I am popup',
+        });
+
+
     return (
         <header>
             <h2 style={{color: 'white'}}>Ruben Gabrielyan</h2>
+            <MainButton text="SHOW POPUP" onClick={handleClick} />
         </header>
     )
 }
